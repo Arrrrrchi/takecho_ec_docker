a1a6c0ced84e1f1ec0fbd4e48116859d4cd8708d
Arrrrrchi <esh6c4nm@gmail.com>
2023-05-18 17:17:46 +0900

    ロゴ index

--- src/resources/views/layouts/admin-navigation.blade.php
+++ src/resources/views/layouts/admin-navigation.blade.php
@@ -4,17 +4,21 @@
         <div class="flex justify-between h-16">
             <div class="flex">
                 <!-- Logo -->
-                <div class="shrink-0 flex items-center">
+                <div class="shrink-0 flex items-center w-20">
                     <a href="{{ route('admin.dashboard') }}">
                         <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
                     </a>
                 </div>
 
                 <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
+                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex ">
                     <x-nav-link :href="route('admin.dashboard')" :active="request()->routeIs('admin.dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    <x-nav-link :href="route('admin.dashboard')" :active="request()->routeIs('admin.dashboard')">
+                        {{ __('Dashboard') }}
+                    </x-nav-link>
+
                 </div>
             </div>
 
--- src/routes/web.php
+++ src/routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('user.welcome');
+    return view('user.index');
 });
 
 Route::get('/dashboard', function () {
