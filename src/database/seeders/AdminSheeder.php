cad904ecf774a245c363058b66a30467e988c25e
Arrrrrchi <esh6c4nm@gmail.com>
2023-05-27 19:46:29 +0900

    シーダー

---
+++ src/database/seeders/AdminSheeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+
+class AdminSheeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('admins')->insert([
+            [
+                'name' => 'admin1',
+                'email' => 'admin1@test.com',
+                'password' => Hash::make('password123'),
+            ],
+        ]);
+    }
+}
