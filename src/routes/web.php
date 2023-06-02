3aa94b851398cdc69e4b59c2a6399da648f58ce2
Arrrrrchi <esh6c4nm@gmail.com>
2023-06-02 12:41:53 +0900

    一時修正

--- src/routes/web.php
+++ src/routes/web.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('user.index');
+    return view('user.welcome');
 });
 
 Route::get('/dashboard', function () {
