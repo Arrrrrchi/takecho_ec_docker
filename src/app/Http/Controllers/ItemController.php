07d797f0e679504530ba2f74474a3e77c08ff617
Arrrrrchi <esh6c4nm@gmail.com>
2023-06-03 14:23:22 +0900

    商品情報

--- src/app/Http/Controllers/ItemController.php
+++ src/app/Http/Controllers/ItemController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Models\Product;
+use App\Models\Stock;
 
 class ItemController extends Controller
 {
@@ -20,6 +21,6 @@ class ItemController extends Controller
 
         $quantity = Stock::where('product_id', $product->id)->sum('quantity');
 
-        return view('items.show', compact(('product')));
+        return view('items.show', compact('product', 'quantity'));
     }
 }
