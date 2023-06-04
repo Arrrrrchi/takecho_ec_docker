f6ae401cff9147efafe86f65b6d4508cd233c268
Arrrrrchi <esh6c4nm@gmail.com>
2023-06-04 09:51:52 +0900

    Cart add

--- src/app/Http/Controllers/User/CartController.php
+++ src/app/Http/Controllers/User/CartController.php
@@ -4,11 +4,27 @@ namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Cart;
+use Illuminate\Support\Facades\Auth;
+
 
 class CartController extends Controller
 {
     public function add (Request $request)
     {
-        dd($request);
+        $itemInCart = Cart::where('product_id', $request->product_id)
+            ->where('user_id', Auth::id())->first();
+
+        if ($itemInCart) {
+            $itemInCart->quantity += $request->quantity;
+            $itemInCart->save();
+        } else {
+            Cart::create([
+                'user_id' => Auth::id(),
+                'product_id' => $request->product_id,
+                'quantity' => $request->quantity,
+            ]);
+        }
+        dd('テスと');
     }
 }
