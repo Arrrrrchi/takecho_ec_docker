0d5008e7549614ad2eefc054b89e853449036639
Arrrrrchi <esh6c4nm@gmail.com>
2023-06-06 10:40:51 +0900

    cartService作成

--- src/app/Http/Controllers/User/CartController.php
+++ src/app/Http/Controllers/User/CartController.php
@@ -8,6 +8,7 @@ use App\Models\Cart;
 use App\Models\User;
 use App\Models\Stock;
 use Illuminate\Support\Facades\Auth;
+use App\Services\CartService;
 
 
 class CartController extends Controller
@@ -55,6 +56,11 @@ class CartController extends Controller
 
     public function checkout ()
     {
+        ////
+        $items = Cart::where('user_id', Auth::id())->get();
+        $products = CartService::getItemsInCart($items);
+        ////
+
         $user = User::findOrfail(Auth::id());
         $products = $user->products;
         $stripe = new \Stripe\StripeClient(env('STRIPE_SECRET_KEY'));
---
+++ src/app/Services/CartService.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Services;
+use App\Models\Product;
+use App\Models\Cart;
+
+
+class CartService
+{
+    public static function getItemsInCart($items)
+    {
+        $products = [];
+
+        foreach($items as $item) {
+            $p = Product::findOrFail($item->product_id);
+            $admin = $p->admin->select('name', 'email')->first()->toArray(); // オーナー情報
+            $values = array_values($admin); // 連想配列の値を取得
+            $keys = ['adminName', 'email'];
+            $adminInfo = array_combine($keys, $values); // オーナー情報のキーを変更
+            
+            $product = Product::where('id', $item->product_id)
+                        ->select('id', 'name', 'price')->get()->toArray(); // 商品情報の配列
+
+            $quantity = Cart::where('product_id', $item->product_id)
+                        ->select('quantity')->get()->toArray(); // 在庫数の配列
+
+            $result = array_merge($product[0], $adminInfo, $quantity[0]); // 配列の結合
+            array_push($products, $result);
+        }  
+        dd($products);
+        return $products;
+    }
+}
