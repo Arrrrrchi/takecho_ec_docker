659bcefa25e455d77b60cafb601877c6d159c112
Arrrrrchi <esh6c4nm@gmail.com>
2023-06-15 13:53:39 +0900

    bg-color

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -13,7 +13,7 @@
     </div>
 
     {{-- ITEMS --}}
-    <div class="relative pt-24 pb-24" style="background-color: RGB(248, 244, 241)">
+    <div class="relative pt-24 pb-24 site-color-1">
         <div class="relative bg-center bg-cover">
             <div class="flex justify-center items-center border-b-2 border-yellow-400 mx-12 pb-4">
                 <h2 class="text-3xl">商品一覧</h2>
@@ -64,7 +64,7 @@
     </div>
 
     {{-- INSTAGRAM --}}
-    <div class="relative pt-24 pb-24"  style="background-color: RGB(248, 244, 241)">
+    <div class="relative pt-24 pb-24 site-color-1">
         <div class="flex justify-center items-center border-b-2 border-yellow-400 mx-12 pb-4">
             <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6">
                 <path stroke-linecap="round" stroke-linejoin="round" d="M5.25 5.653c0-.856.917-1.398 1.667-.986l11.54 6.348a1.125 1.125 0 010 1.971l-11.54 6.347a1.125 1.125 0 01-1.667-.985V5.653z" />
