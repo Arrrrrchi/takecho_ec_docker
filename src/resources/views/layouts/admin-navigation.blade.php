93be0149815956f84153ea0c82873dd94294f54e
Arrrrrchi <esh6c4nm@gmail.com>
2023-07-27 15:04:10 +0900

    ログイン画面のヘッダー削除

--- src/resources/views/layouts/admin-navigation.blade.php
+++ src/resources/views/layouts/admin-navigation.blade.php
@@ -1,21 +1,17 @@
-<nav x-data="{ open: false }" class="bg-white border-b border-gray-100">
+<nav x-data="{ open: false }" class="bg-green-100 border-b border-gray-100">
     <!-- Primary Navigation Menu -->
     <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div class="flex justify-between h-16">
             <div class="flex">
-                <p>A</p>
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center w-20">
-                    <a href="{{ route('admin.dashboard') }}">
+                    <a href="{{ route('admin.products.index') }}">
                         <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
                     </a>
                 </div>
 
                 <!-- Navigation Links -->
                 <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex ">
-                    <x-nav-link :href="route('admin.dashboard')" :active="request()->routeIs('admin.dashboard')">
-                        {{ __('Dashboard') }}
-                    </x-nav-link>
                     <x-nav-link :href="route('admin.products.index')" :active="request()->routeIs('admin.products.index')">
                         商品管理
                     </x-nav-link>
@@ -75,9 +71,6 @@
     <!-- Responsive Navigation Menu -->
     <div :class="{'block': open, 'hidden': ! open}" class="hidden sm:hidden">
         <div class="pt-2 pb-3 space-y-1">
-            <x-responsive-nav-link :href="route('admin.dashboard')" :active="request()->routeIs('admin.dashboard')">
-                {{ __('Dashboard') }}
-            </x-responsive-nav-link>
             <x-responsive-nav-link :href="route('admin.products.index')" :active="request()->routeIs('admin.products.index')">
                 商品管理
             </x-responsive-nav-link>
