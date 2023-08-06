6b7e139cbbcbe8101a63cb50e3d4ea6559d99427
Arrrrrchi <esh6c4nm@gmail.com>
2023-08-07 07:00:40 +0900

    ControllerTestの作成

---
+++ src/tests/Feature/ControllerTest.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class ControllerTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function testAbout(): void
+    {
+        $response = $this->get(route('about'));
+
+        $response->assertStatus(200)
+            ->assertViewIs('about');
+    }
+
+    public function testNameko(): void
+    {
+        $response = $this->get(route('nameko'));
+
+        $response->assertStatus(200)
+            ->assertViewIs('nameko');
+    }
+
+    public function testPolicy(): void
+    {
+        $response = $this->get(route('policy'));
+
+        $response->assertStatus(200)
+            ->assertViewIs('policy');
+    }
+
+    public function testLaw(): void
+    {
+        $response = $this->get(route('law'));
+
+        $response->assertStatus(200)
+            ->assertViewIs('specific_trade_law');
+    }
+
+    public function testTerms(): void
+    {
+        $response = $this->get(route('terms'));
+
+        $response->assertStatus(200)
+            ->assertViewIs('terms');
+    }
+
+}
